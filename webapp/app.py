5fe6210055f8c87f8cb6755304464cde6da9fd88
cc090206 <chowchloe5@gmail.com>
2025-05-08 00:32:14 -0700

    Split html to css for webapp
    Finished May 6th

--- webapp/app.py
+++ webapp/app.py
@@ -1,24 +1,6 @@
-from flask import Flask, render_template, request
-import RPi.GPIO as GPIO
-import time
+from flask import Flask, render_template
 
 app = Flask(__name__)
-
-IN1, IN2, IN3, IN4, ENA, ENB = 1, 27, 23, 24, 22, 25
-GPIO.setmode(GPIO.BCM)
-GPIO.setup([IN1, IN2, IN3, IN4, ENA, ENB], GPIO.OUT)
-
-pwm_left = GPIO.PWM(ENA, 1000)
-pwm_right = GPIO.PWM(ENB, 1000)
-pwm_left.start(0)
-pwm_right.start(0)
-
-speed = 80
-
-def stop():
-    GPIO.output([IN1, IN2, IN3, IN4], GPIO.LOW)
-    pwm_left.ChangeDutyCycle(0)
-    pwm_right.ChangeDutyCycle(0)
 
 @app.route("/")
 def login():
@@ -39,36 +21,6 @@ def error():
 @app.route("/supplies")
 def supplies():
     return render_template("supplies.html")
-
-@app.route("/move", methods=["POST"])
-def move():
-    direction = request.form.get("direction")
-    if direction == "forward":
-        GPIO.output(IN1, GPIO.HIGH)
-        GPIO.output(IN2, GPIO.LOW)
-        GPIO.output(IN3, GPIO.HIGH)
-        GPIO.output(IN4, GPIO.LOW)
-    elif direction == "backward":
-        GPIO.output(IN1, GPIO.LOW)
-        GPIO.output(IN2, GPIO.HIGH)
-        GPIO.output(IN3, GPIO.LOW)
-        GPIO.output(IN4, GPIO.HIGH)
-    elif direction == "left":
-        GPIO.output(IN1, GPIO.LOW)
-        GPIO.output(IN2, GPIO.HIGH)
-        GPIO.output(IN3, GPIO.HIGH)
-        GPIO.output(IN4, GPIO.LOW)
-    elif direction == "right":
-        GPIO.output(IN1, GPIO.HIGH)
-        GPIO.output(IN2, GPIO.LOW)
-        GPIO.output(IN3, GPIO.LOW)
-        GPIO.output(IN4, GPIO.HIGH)
-    elif direction == "stop":
-        stop()
-
-    pwm_left.ChangeDutyCycle(speed)
-    pwm_right.ChangeDutyCycle(speed)
-    return "OK", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
