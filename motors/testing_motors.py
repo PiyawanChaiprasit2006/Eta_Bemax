5d49c24388473b73440132f5964966ffe73c41d6
cc090206 <chowchloe5@gmail.com>
2025-05-10 00:42:39 -0700

    Updated testing motor with new var names
    Added code for the first motor to run

--- motors/testing_motors.py
+++ motors/testing_motors.py
@@ -42,26 +42,40 @@ import RPi.GPIO as GPIO
 # Set up GPIO pins with BCM pin numbering
 GPIO.setmode(GPIO.BCM)
 
+ENA = 18 # PWN pin (motor 1)
+IN1 = 23 # GPIO23 -> Pin 16
+IN2 = 24 # GPIO24 -> Pin 18
+
 # Motor control pins
-GPIO.setup(12, GPIO.OUT)  # ENA (PWM pin)
-GPIO.setup(23, GPIO.OUT)  # IN1 (GPIO23 -> Pin 16)
-GPIO.setup(24, GPIO.OUT)  # IN2 (GPIO24 -> Pin 18)
+GPIO.setup(ENA, GPIO.OUT)  # ENA (PWM pin)
+GPIO.setup(IN1, GPIO.OUT)  # IN1 (GPIO23 -> Pin 16)
+GPIO.setup(IN2, GPIO.OUT)  # IN2 (GPIO24 -> Pin 18)
+
+pwm1 = GPIO.PWN(ENA, 1000) # 1kHz PWN
+pwm1.start(0) # Start at 0% (stopped)
 
 try:
     print("Testing motor direction...")
 
     # Test clockwise rotation (IN1 HIGH, IN2 LOW)
-    GPIO.output(23, GPIO.HIGH)  # Set IN1 (GPIO23 -> Pin 16)
-    GPIO.output(24, GPIO.LOW)   # Set IN2 (GPIO24 -> Pin 18)
+    GPIO.output(IN1, GPIO.HIGH)  # Set IN1 (GPIO23 -> Pin 16)
+    GPIO.output(IN2, GPIO.LOW)   # Set IN2 (GPIO24 -> Pin 18)
+    for dc in range(0, 101, 10): # should slowly increase the speed of the motor
+        pwm1.ChangeDutyCycle(dc)
+        time.sleep(0.2)
     print("Motor should rotate clockwise now...")
     time.sleep(3)
 
     # Test counterclockwise rotation (IN1 LOW, IN2 HIGH)
-    GPIO.output(23, GPIO.LOW)   # Set IN1 (GPIO23 -> Pin 16)
-    GPIO.output(24, GPIO.HIGH)  # Set IN2 (GPIO24 -> Pin 18)
+    GPIO.output(IN1, GPIO.LOW)   # Set IN1 (GPIO23 -> Pin 16)
+    GPIO.output(IN2, GPIO.HIGH)  # Set IN2 (GPIO24 -> Pin 18)
+    for dc in range(0, 101, 10): # should slowly increase the speed of the motor
+        pwm1.ChangeDutyCycle(dc)
+        time.sleep(0.2)
     print("Motor should rotate counterclockwise now...")
     time.sleep(3)
 
 finally:
+    pwm1.stop()
     GPIO.cleanup()  # Clean up GPIO settings
     print("GPIO cleaned up.")
